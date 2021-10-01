c4c39252655d91cefc8e92fe66162d595352e526
Jack <x@x.com>
2021-10-01 21:55:52 +0800

    quick bug fixed

--- src/Details/xbmp_std_image_loader.cpp
+++ src/Details/xbmp_std_image_loader.cpp
@@ -5,14 +5,14 @@ namespace xbmp::tools::loader
 {
     error* LoadSTDImage(xcore::bitmap& Bitmap, const char* pFileName) noexcept
     {
-        int nBPP;
+        int nChannels;
         int W, H;
         unsigned char* pData;
 
-        if( pData = stbi_load(pFileName, &W, &H, &nBPP, 4 ); nullptr == pData )
+        if( pData = stbi_load(pFileName, &W, &H, &nChannels, 0 ); nullptr == pData )
             return error_code< error::FAILURE, error_str("Fail to load image") >;
 
-        const auto DataSize      = (W * H * nBPP) / 8;
+        const auto DataSize      = (W * H * nChannels);
         const auto TotalDataSize = DataSize + sizeof(int);
 
         auto FinalData = std::make_unique<std::byte[]>(TotalDataSize);
@@ -27,9 +27,9 @@ namespace xbmp::tools::loader
 
 
         xcore::bitmap::format Fmt;
-        switch(nBPP)
+        switch(nChannels*8)
         {
-        case 32: Fmt = xcore::bitmap::format::A8R8G8B8; break;
+        case 32: Fmt = xcore::bitmap::format::R8G8B8A8; break;
         case 24: Fmt = xcore::bitmap::format::R8G8B8; break;
         case 16: Fmt = xcore::bitmap::format::R5G6B5; break;
         default:
